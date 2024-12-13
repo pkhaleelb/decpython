a90328045c3a67f6f80c031cd4cd012f6b5d818b
pkhaleelb <pkbasha143@gmail.com>
2024-12-13 09:19:06 +0530

    ok

--- for loop.py
+++ for loop.py
@@ -1,3 +1,17 @@
 # range (stop)
 for x in range(5):
     print(x)
+
+#range (start,stop
+for x in range (1000,1007):
+    print(x)
+
+#range (start,stop,step)
+for x in range(2,22,2):
+    print(x)
+
+for x in range(10):
+    if x%2==0:
+        print(x,"even")
+    else:
+        print(x,"odd")
