491fa07988e6d576b0579162bd340cc82d20cf14
pkhaleelb <pkbasha143@gmail.com>
2024-12-16 23:36:26 +0530

    Update 16dec.py

--- 16dec.py
+++ 16dec.py
@@ -6,20 +6,3 @@
 # else:
 #     #print('number is zero')
 
-maths=int(input('enter maths marks:'))
-phy=int(input('enter phy marks:'))
-chem=int(input('enter chem marks:'))
-total_marks=600
-sum_marks=maths+phy+chem
-per=(sum_marks/total_marks)*100
-print(per)
-if per<35:
-    print('grade is f')
-elif per>35 and per<=50:
-    print('grade is c')
-elif per in range (51,91):
-    print('grade is b')
-elif per in range (91,101):
-    print('grade is a')
-else:
-    print('invalid per')
