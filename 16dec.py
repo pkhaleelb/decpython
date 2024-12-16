c1697b2c9068a40ca42ad47f0d281aff5b791212
pkhaleelb <pkbasha143@gmail.com>
2024-12-16 23:50:51 +0530

    Update 16dec.py

--- 16dec.py
+++ 16dec.py
@@ -1,8 +1,7 @@
-# num=int(input('enter number:'))
-# if num<0:
-#     print('-ve number')
-# elif num>0:
-#     print('+ve number')
-# else:
-#     #print('number is zero')
-
+num=int(input('enter number:'))
+if num<0:
+    print('-ve number')
+elif num>0:
+   print('+ve number')
+else:
+   print('number is zero')
