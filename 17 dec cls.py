797045188c83c38a3f86a446a89893d54b8b4ff2
pkhaleelb <pkbasha143@gmail.com>
2024-12-17 09:16:03 +0530

    Update 17 dec cls.py

--- 17 dec cls.py
+++ 17 dec cls.py
@@ -9,3 +9,19 @@
 #         print('75k')
 # else:
 #     print('not allowed')
+
+# while True:
+#     print('1.payment failed')
+#     print('2.profile and privacy')
+#     print('3.money transfer')
+#     print('exit')
+#     User_inp=int(input('enter ur choice'))
+#     if User_inp==1:
+#         print('welcome to payment failed ')
+#     elif User_inp==2:
+#         print('welcome to profile and privacy')
+#     elif User_inp==3:
+#         print('welcome to money transfer')
+#     elif User_inp==4:
+#         print('exit')
+#         break
