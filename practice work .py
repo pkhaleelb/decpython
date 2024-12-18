a56597c88be8d101108213d554ad74fa1de66fac
pkhaleelb <pkbasha143@gmail.com>
2024-12-18 22:59:38 +0530

    Create practice work .py

---
+++ practice work .py
@@ -0,0 +1,63 @@
+# print('hello world')
+
+# calculating simple interest 
+
+# p=1000
+# t=2
+# r=15
+# si=(p*t*r)/100
+# print('rupees of si',si)
+
+# Adding the values & printing the values
+
+# a=150
+# b=750
+# c=650
+# addition=a+b+c
+# print('sum of add:',addition)
+
+# Calculate the age of a person. take a value from the user
+
+# a=int(input('enter the birth year:'))
+# b=int(input('enter current year:'))
+# age=b-a
+# print('age of a person:',age)
+
+# Calculate the average from the user
+
+# a=123
+# b=456
+# c=789
+# average=a+b+c/3
+# print(average)
+
+# There are 2 persons height & their name & Check who is more greater in height
+#                      {IF ELSE}
+
+# first_person_name=input('enter name:')
+# second_person_name=input('enter name:')
+# height_1=float(input(f'enter{first_person_name} height:'))
+# height_2=float(input(f'enter{second_person_name} height:'))
+# if height_1>height_2:
+#     print(first_person_name,'height is more')
+# else:
+#     print(second_person_name,'height is more')
+
+# first_person_name=input('enter name:')
+# second_person_name=input('enter name:')
+# height_1=float(input(f'enter{first_person_name}height:'))
+# height_2=float(input(f'enter{second_person_name}height:'))
+# if height_1>height_2:
+#     print(first_person_name,'height is more')
+# else:
+#     print(second_person_name,'height is more')
+
+# number=int(input('enter number:'))
+# if number>0:
+#     print(number,'positive')
+# elif number==0:
+#     print(number,'neither positive nor negative')
+# else:
+#     print(number,'negative')
+
+ 
