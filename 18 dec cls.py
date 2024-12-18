7998820347d3dbb819f9ba85fb1e18a01662d342
pkhaleelb <pkbasha143@gmail.com>
2024-12-18 09:06:57 +0530

    Create 18 dec cls.py

---
+++ 18 dec cls.py
@@ -0,0 +1,33 @@
+# count=0
+# value='arjun'
+# for x in value:
+#     print(x)
+#     count=count+1
+#     print(count)
+
+# count=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+# print('totalvalues of odd',count)
+
+# count=0
+# sum=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+# print('totalvalues of odd',count)
+# print('total sumof odd',sum)
+
+# count=0
+# sum=0
+# product=1
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+#     product=product*x
+# print('totalvalues of odd',count)
+# print('total sumof odd',sum)
+# print('total product of odd',product) 
