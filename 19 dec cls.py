2cdf0e06bc22daae5652b2edfad6093f802923ee
pkhaleelb <pkbasha143@gmail.com>
2024-12-20 08:17:13 +0530

    Create 19 dec cls.py

---
+++ 19 dec cls.py
@@ -0,0 +1,27 @@
+# val="rama"
+# print(val[2])
+
+# #1000=-9-8-7-6-5-4-3-2-1
+# sales='rameshwar'
+# #1000 =012345678
+# print(sales[6:7])
+# print(sales[-3:-1])
+
+# #1000=-9-8-7-6-5-4-3-2-1
+# sales='tameshwar'
+# #1000 =012345678
+# print(sales[6:7])
+# print(sales[::1])
+# print(sales[::-1])
+
+# #1000=-6-5-4-3-2-1
+# sales='nithin'
+# #1000 =012345
+# print(sales[::1])
+# print(sales[::-1])
+
+# val='nitin'
+# if val[::1]==val[::-1]:
+#     print('palindrome')
+# else:
+#     print('not palindrome')
