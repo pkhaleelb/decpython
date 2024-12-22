a29d58b430d7592090464305aeaf4fb67b92fab7
pkhaleelb <pkbasha143@gmail.com>
2024-12-22 19:44:49 +0530

    Create 20 dec cls.py

---
+++ 20 dec cls.py
@@ -0,0 +1,37 @@
+# upper case
+# print('hello😍'.upper())
+
+# lower case
+# print('HELLO😊'.lower())
+
+#ordinal number
+
+# print('A','ord--->',ord('A'))
+# print('a','ord--->',ord('a'))
+
+# print(65,'chr--->',chr(65))
+# print(97,'chr--->',chr(97))
+
+# val='ABC'
+# for x in val:
+#     ord_val=ord(x)
+#     conv_chr=ord_val+32
+#     print(chr(conv_chr))
+
+# val='abc'
+# for x in val:
+#      ord_val=ord(x)
+#      conv_chr=ord_val-32
+#      print(chr(conv_chr))
+
+# swapcase
+
+# val='ABc$TuuO'
+# for x in val:
+#     ord_val=ord(x)
+#     if ord_val in range (65,91):
+#         print(chr(ord_val+32),end='')
+#     elif ord_val in range (97,123):
+#         print(chr(ord_val-32),end='') 
+#     else:
+#         print(x,end='')
